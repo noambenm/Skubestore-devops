68316901d18564a5f467b57d922a515fe12002ec
Noam Ben Mordechai <noam112113@gmail.com>
2024-12-19 15:54:11 +0200

    removed the internal ingress and switched to dircrly calling backend services

--- running_the_app/cleanup.sh
+++ running_the_app/cleanup.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-kubectl delete ingress --all -A
-kubectl delete service --all -A
 kubectl delete -f ../k8s/aws-ingress-controller.yml
 kubectl delete -f ../k8s/external-dns.yml
 kubectl delete -f ../k8s/frontend-deployment.yml
-kubectl delete -f ../k8s/internal-ingress.yml
 kubectl delete -f ../k8s/order-deployment.yml
 kubectl delete -f ../k8s/product-deployment.yml
 kubectl delete -f ../k8s/user-deployment.yml
