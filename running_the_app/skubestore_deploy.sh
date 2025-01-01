a38b75f8aa4c136d46563d49119179ee40453c37
Noam Ben Mordechai <noam112113@gmail.com>
2025-01-02 01:23:36 +0200

    Remove internal ingress configuration and related installation steps from deployment script

--- running_the_app/skubestore_deploy.sh
+++ running_the_app/skubestore_deploy.sh
@@ -27,31 +27,11 @@ sleep 10
 git clone https://github.com/noambenm/Skubestore-devops.git
 cd Skubestore-devops/k8s
 
-# Installing ngnix ingress controller
-echo "Installing ngnix ingress controller..."
-helm upgrade --install ingress-nginx ingress-nginx \
-  --repo https://kubernetes.github.io/ingress-nginx \
-  --namespace ingress-nginx --create-namespace \
-  --set controller.service.type=ClusterIP || error_exit "Failed to install ngnix ingress controller."
-
-# Wait for NGINX ingress controller webhook to become ready
-echo "Waiting for NGINX ingress controller webhook to become ready..."
-while true; do
-    kubectl get endpoints ingress-nginx-controller-admission -n ingress-nginx | grep -q "10." && break
-    echo "Waiting for ingress-nginx-admission to become ready..."
-    sleep 5
-done
-echo "NGINX ingress controller webhook is ready."
-sleep 10
-
 # Installing kubernetes components
 echo "Installing kubernetes components..."
 kubectl apply -f namespace.yml
 kubectl apply -f aws-ingress-controller.yml
 kubectl apply -f external-dns.yml
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.1/cert-manager.yaml
-kubectl apply -f cluster-issuer.yml
-kubectl apply -f internal-ingress.yml
 # Install stage environment
 
 kubectl config set-context --current --namespace=skubestore-stage
